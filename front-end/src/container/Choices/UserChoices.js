38636860d1fe740c5905898166475b1671271686
Yonatan <48679462+haverzard@users.noreply.github.com>
2020-06-02 14:50:04 +0700

    Adding frontend base page

---
+++ front-end/src/container/Choices/UserChoices.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Choice.css';
+
+export default function Home() {
+    return (
+        <div className="Choice-flex">
+            <div className="Choice-box">
+                Create a new crowd-funding project
+            </div>
+            <div className="Choice-box">
+                Fund a project
+            </div>
+        </div>
+    )
+}
---
+++ front-end/src/container/Home/Home.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+import {CSSTransition} from 'react-transition-group';
+import logo from './money.svg';
+import './App.css';
+import UserChoices from '../Choices/UserChoices.js'
+
+export default function Home() {
+    const [enabled, setEnabled] = useState(false)
+    useEffect(() => {
+        document.addEventListener("keydown", (e) => { if (e.keyCode == 40) setEnabled(true) })
+        return () => {
+        document.removeEventListener("keydown", (e) => { if (e.keyCode == 40) setEnabled(true) }, false);
+        }
+    })
+    return (
+        <header className="App-header">
+            <CSSTransition
+                in={!enabled}
+                timeout={350}
+                classNames="display"
+                unmountOnExit
+                appear
+                >
+                <div>
+                <img src={logo} className="App-logo" alt="logo" />
+                </div>
+            </CSSTransition>
+            <p className="App-title">
+                CrowdFunding Platform
+            </p>
+            <p className="App-link">
+                Create your own crowd funding projects here!
+            </p>
+            {!enabled && 
+                <div id="App-arrow-container" onClick={() => setEnabled(true)}>
+                    <div style={{opacity: 0.2}}>press key down or click this</div>
+                    <span id="App-arrow"></span>
+                </div>
+            }
+            <CSSTransition
+                in={enabled}
+                timeout={350}
+                classNames="display"
+                unmountOnExit
+                appear
+                >
+                <UserChoices />
+            </CSSTransition>
+        </header>
+    )
+}
